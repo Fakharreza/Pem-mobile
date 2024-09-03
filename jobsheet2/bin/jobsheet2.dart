a2c220a78fa5355979f5071dfab8549bf4d26c51
Reza alamsyah <reza.alamsyah2013@gmail.com>
2024-09-03 13:00:13 +0700

    first commit

---
+++ jobsheet2/bin/jobsheet2.dart
@@ -0,0 +1,7 @@
+import 'package:jobsheet2/jobsheet2.dart' as jobsheet2;
+
+void main(List<String> arguments) {
+  for (var i = 18; i > 8; i--) {
+    print('Nama Saya Adalah Fulan , sekarang saya berumur ${i + 0}');
+}
+}
